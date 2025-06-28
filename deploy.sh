315d645000fcb0e41e8edfd64e8de8338df92025
kris <kris.raich@gmail.com>
2025-06-28 18:35:56 +0200

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 # push to https://hub.docker.com/r/krisraich/simplea/
-mvn clean package && docker build -f src/main/docker/Dockerfile.jvm -t krisraich/simplea . && docker push
+mvn clean package && docker build -f src/main/docker/Dockerfile.jvm -t krisraich/simplea . && docker push krisraich/simplea:latest
